b2ef72ca284ec23b0083e14ed50120cb264b5aa3
yeeuniii <anna862700@gmail.com>
2024-07-09 01:24:08 +0900

    solve/#1: 백준 1158 요세푸스 문제 풀이 - deque로

--- baekjoon/1158-요세푸스 문제/solve.py
+++ baekjoon/1158-요세푸스 문제/solve.py
@@ -1,45 +1,24 @@
 from collections import deque
 import sys
 
-
-class Queue:
-    def __init__(self):
-        self.queue = deque()
-        self.size = 0
-
-    def enqueue(self, element):
-        self.queue.append(element)
-        self.size += 1
-
-    def dequeue(self):
-        if not self.empty():
-            self.size -= 1
-            return self.queue.popleft()
-        return None
-
-    def empty(self):
-        return self.size == 0
-
-
 n, k = map(int, sys.stdin.readline().strip().split())
-res = list()
-empty_queue = Queue()
-filled_queue = Queue()
+result = list()
+empty_queue = deque()
+filled_queue = deque()
 for idx in range(n):
-    filled_queue.enqueue(idx + 1)
+    filled_queue.append(idx + 1)
 
-
-while len(res) < n:
+while len(result) < n:
     for idx in range(k - 1):
-        if filled_queue.empty():
+        if len(filled_queue) == 0:
             filled_queue = empty_queue
-            empty_queue = Queue()
-        empty_queue.enqueue(filled_queue.dequeue())
-    if filled_queue.empty():
+            empty_queue = deque()
+        empty_queue.append(filled_queue.popleft())
+    if len(filled_queue) == 0:
         filled_queue = empty_queue
-        empty_queue = Queue()
-    res.append(filled_queue.dequeue())
+        empty_queue = deque()
+    result.append(filled_queue.popleft())
 
 print("<", end="")
-print(*res, sep=", ", end="")
+print(*result, sep=", ", end="")
 print(">", end="")
