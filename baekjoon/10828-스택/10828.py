4fc80ee94a4096a2f621fcba1625000e2ca7eca8
yeeuniii <anna862700@gmail.com>
2024-07-08 21:41:09 +0900

    solve/#1: 백준 10828 스택 문제 풀이 및 정리

--- baekjoon/10828-스택/10828.py
+++ baekjoon/10828-스택/10828.py
@@ -4,37 +4,50 @@ import sys
 class Stack:
     def __init__(self):
         self.stack = list()
-        self.size = 0
+        self.ssize = 0
 
     def push(self, element):
         self.stack.append(element)
-        self.size += 1
+        self.ssize += 1
 
     def pop(self):
         num = -1
-        if not self.empty():
+        if self.ssize > 0:
             num = self.stack.pop()
-            self.size -= 1
+            self.ssize -= 1
         print(num)
 
-
     def size(self):
-        print(self.size)
-
+        print(self.ssize)
 
     def empty(self):
         is_empty = True
-        if self.size > 0:
+        if self.ssize > 0:
             is_empty = False
         print(int(is_empty))
-        return is_empty
 
     def top(self):
         num = -1
-        if not self.empty():
-            num = self.stack[self.size - 1]
+        if self.ssize > 0:
+            num = self.stack[self.ssize - 1]
         print(num)
 
 
 n = int(sys.stdin.readline())
+cmds = list(sys.stdin.readline().strip().split() for _ in range(n))
 
+stack = Stack()
+funcs = {
+    "pop": stack.pop,
+    "size": stack.size,
+    "empty": stack.empty,
+    "top": stack.top
+}
+
+for idx in range(n):
+    cmd = cmds[idx][0]
+    if cmd == "push":
+        num = int(cmds[idx][1])
+        stack.push(num)
+    else:
+        funcs[cmd]()
