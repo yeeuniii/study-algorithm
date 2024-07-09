f963cd6a029049d1d9f5f971917ebad4e387b0ee
yeeuniii <anna862700@gmail.com>
2024-07-09 14:47:05 +0900

    solve/#1: 백준 1158 요세푸스 문제 풀이 - 리스트 인덱스 접근

--- baekjoon/1158-요세푸스 문제/solve.py
+++ baekjoon/1158-요세푸스 문제/solve.py
@@ -1,23 +1,15 @@
-from collections import deque
 import sys
 
 n, k = map(int, sys.stdin.readline().strip().split())
+lst = list(range(1, n + 1))
 result = list()
-empty_queue = deque()
-filled_queue = deque()
-for idx in range(n):
-    filled_queue.append(idx + 1)
+idx = 0
 
-while len(result) < n:
-    for idx in range(k - 1):
-        if len(filled_queue) == 0:
-            filled_queue = empty_queue
-            empty_queue = deque()
-        empty_queue.append(filled_queue.popleft())
-    if len(filled_queue) == 0:
-        filled_queue = empty_queue
-        empty_queue = deque()
-    result.append(filled_queue.popleft())
+for _ in range(n):
+    idx += k - 1
+    if idx >= len(lst):
+        idx %= len(lst)
+    result.append(lst.pop(idx))
 
 print("<", end="")
 print(*result, sep=", ", end="")
