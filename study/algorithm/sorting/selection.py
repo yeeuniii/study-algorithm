3dbc573739d74dd9fdf83cd979930a1c0e5a9ceb
yeeuniii <anna862700@gmail.com>
2024-07-31 01:09:13 +0900

    study/#6: 선택정렬 코드 구현

---
+++ study/algorithm/sorting/selection.py
@@ -0,0 +1,22 @@
+def get_minimum(data):
+    minimum = data[0]
+    minimum_index = 0
+
+    for index, value in enumerate(data):
+        if value < minimum:
+            minimum = value
+            minimum_index = index
+    return minimum, minimum_index
+
+
+def selection_sort(data):
+    for index in range(len(data)):
+        minimum, minimum_index = get_minimum(data[index:])
+
+        data[minimum_index + index] = data[index]
+        data[index] = minimum
+    return data
+
+
+data = [1, -1, -2, 2, -3, 3, 0]
+print(selection_sort(data))
