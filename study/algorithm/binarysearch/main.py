033e393bf98c44fbf1769f8231824d84dd8df60d
yeeuniii <anna862700@gmail.com>
2024-07-31 16:05:58 +0900

    study/#6: 이진 탐색 재귀로 구현

--- study/algorithm/binarysearch/main.py
+++ study/algorithm/binarysearch/main.py
@@ -1,4 +1,5 @@
 from study.algorithm.binarysearch.binary_search import binary_search
+from study.algorithm.binarysearch.binary_search_recursive import recursive_binary_search
 
 
 def main():
@@ -7,6 +8,9 @@ def main():
 
     print(binary_search(target, data))
     print(binary_search(max(data) + 1, data))
+
+    print(recursive_binary_search(target, data, 0, len(data) - 1))
+    print(recursive_binary_search(max(data) + 1, data, 0, len(data) - 1))
 
 
 if __name__ == "__main__":
